ba31bc67672c545b0b94c65cfa774654e94a6ba3
prashant kumar dubey <prashantdubey194@gmail.com>
2015-10-16 17:36:58 +0530

    Grade.java

---
+++ com/prashant/multilist/Grade.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.prashant.multilist;
+
+/**
+ * @author p00318395
+ *
+ */
+public class Grade {
+	
+	private Integer gradeID;
+	
+	private String gradeName;
+	
+	public Integer getGradeID() {
+		return gradeID;
+	}
+
+	public void setGradeID(Integer gradeID) {
+		this.gradeID = gradeID;
+	}
+
+	public String getGradeName() {
+		return gradeName;
+	}
+
+	public void setGradeName(String gradeName) {
+		this.gradeName = gradeName;
+	}
+
+	
+
+}
